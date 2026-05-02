04692ae3c3e5f0da5e45d6ad57740a5581958a4a
NguyenHongPhuc-2455 <hongphuc092004@gmail.com>
2026-05-02 21:10:19 +0700

    feat: implement LessonPlayer component to handle video and quiz content rendering with access control

--- frontend/securityVideo/src/pages/client/CourseLearning/components/LessonPlayer.tsx
+++ frontend/securityVideo/src/pages/client/CourseLearning/components/LessonPlayer.tsx
@@ -46,8 +46,9 @@ export default function LessonPlayer({
 
         if (activeLesson.video_url.includes('.m3u8')) {
             const isAbsolute = activeLesson.video_url.startsWith('http');
-            const finalSrc = isAbsolute ? activeLesson.video_url : `http://localhost:5000${activeLesson.video_url}`;
-
+            // const finalSrc = isAbsolute ? activeLesson.video_url : `http://localhost:5000${activeLesson.video_url}`;
+            const BASE_URL = import.meta.env.VITE_API_URL?.replace('/api', '') || 'http://localhost:5000';
+            const finalSrc = isAbsolute ? activeLesson.video_url : `${BASE_URL}${activeLesson.video_url}`;
             return (
                 <VideoPlayer
                     key={activeLesson.id}
